ba14859b6841658699f8e2699f45e0189acba64f
Naren1520 <Narensonu1520@gmail.com>
2025-12-28 13:40:42 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
-      "/auth": "http://localhost:5000",
+      "/api": "http://localhost:8000",
+      "/auth": "http://localhost:8000",
     },
   },
 });
